11bdb1161f2f0e710608e40170347bfeecef0019
MahidulIslam1 <108355059+MahidulIslam1@users.noreply.github.com>
2023-09-14 22:19:27 -0400

    added images

---
+++ index.js
@@ -0,0 +1,49 @@
+document.getElementById('startButton').addEventListener('click', function(event) {
+  event.preventDefault();
+  const targetElement = document.getElementById('start');
+  targetElement.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+
+const carousel = document.getElementById('carousel');
+const images = [
+  "sumama.jpeg",
+  "sardar.png",
+  "zubayer.png",
+  "adam.png",
+  "mahidul.png",
+  "faiyaz.png",
+  "samen.png",
+  "jojo.png"
+];
+const numberOfImages = images.length;
+let currentImageIndex = 0;
+
+function updateCarousel() {
+  const previousIndex = (currentImageIndex - 1 + numberOfImages) % numberOfImages;
+  const nextIndex = (currentImageIndex + 1) % numberOfImages;
+
+  document.getElementById('previous').children[0].src = images[previousIndex];
+  document.getElementById('current').children[0].src = images[currentImageIndex];
+  document.getElementById('next').children[0].src = images[nextIndex];
+}
+
+setInterval(() => {
+  currentImageIndex = (currentImageIndex + 1) % numberOfImages;
+  updateCarousel();
+}, 4000); 
+
+updateCarousel(); 
+
+
+
+
+
+
+
+
+
+
+
+
